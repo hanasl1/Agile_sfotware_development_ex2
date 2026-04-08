15718d129c2636082168f3b390785236288d2254
Hana Šlipogor <hana.slipogor@gmail.com>
2026-04-08 17:17:05 +0200

    Add /registered/{id} endpoint to show user detail page

--- p2-todolist-app/src/main/java/todolist/controller/HomeController.java
+++ p2-todolist-app/src/main/java/todolist/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import todolist.service.UsuarioService;
 
 @Controller
@@ -21,5 +22,11 @@ public class HomeController {
     public String showRegisteredUsers(Model model) {
         model.addAttribute("users", usuarioService.findAll());
         return "registeredUsers";
+    }
+
+    @GetMapping("/registered/{id}")
+    public String showRegisteredUserDescription(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", usuarioService.findById(id));
+        return "registeredUserDescription";
     }
 }
