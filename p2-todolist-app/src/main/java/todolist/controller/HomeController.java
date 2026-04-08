357fd1ec9c8a289c9073a292eeb0bd1322d4e338
Hana Šlipogor <hana.slipogor@gmail.com>
2026-04-08 13:29:13 +0200

    Add /registered endpoint to show registered user list

--- p2-todolist-app/src/main/java/todolist/controller/HomeController.java
+++ p2-todolist-app/src/main/java/todolist/controller/HomeController.java
@@ -1,13 +1,25 @@
 package todolist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import todolist.service.UsuarioService;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/about")
     public String about(Model model) {
         return "about";
+    }
+
+    @GetMapping("/registered")
+    public String showRegisteredUsers(Model model) {
+        model.addAttribute("users", usuarioService.findAll());
+        return "registeredUsers";
     }
 }
